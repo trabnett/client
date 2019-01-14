86459ac923215c6a1423c1aaea8c2f266239af02
trabnett <timrabnett@gmail.com>
2019-01-14 16:54:44 +0000

    client project. added function that can buffer chunks of data

---
+++ getAndPrintBuffered.js
@@ -0,0 +1,20 @@
+var https = require('https')
+
+function getAndPrintHTML () {
+  var arr = "";
+  var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step2.html'
+  };
+
+  https.get(requestOptions, function (response){
+    response.setEncoding('utf8');
+
+    response.on('data', function (data) {
+      arr += data
+      console.log("data chunk", arr);
+    });
+  })
+
+}
+console.log(getAndPrintHTML())
